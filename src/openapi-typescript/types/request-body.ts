f8f041c09fa4598e30c201a437ba7c056f9ee61c
mnorlin <1668384+mnorlin@users.noreply.github.com>
2023-03-13 09:58:57 +0100

    feat: initial commit

---
+++ src/openapi-typescript/types/request-body.ts
@@ -0,0 +1,11 @@
+/**
+ * Extract the request body from an <i>Operation</i>.
+ */
+export type OperationRequestBody<
+  Operation,
+  ContentType extends string = string
+> = Operation extends {
+  requestBody?: { content: { [key in ContentType]: infer RequestBody } };
+}
+  ? RequestBody
+  : never;
